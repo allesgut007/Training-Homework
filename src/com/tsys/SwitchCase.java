d0a4033af62ddacd8905edef895b74479ee88782
alles <alles@SRIKARA>
2021-08-04 07:07:48 +0530

    as on 3/8

---
+++ src/com/tsys/SwitchCase.java
@@ -0,0 +1,38 @@
+package com.tsys;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+
+		int day = 3; 		
+
+		switch (day) {
+
+		case 1:
+
+			System.out.println("today is Monday");
+
+			break;
+
+		case 2:
+
+			System.out.println("today is Tuesday");
+
+			break;
+
+		case 3:
+
+			System.out.println("today is Wednesday");
+
+			break;
+
+		default:
+
+			System.out.println("Assume its Sunday :)");
+
+
+		}
+
+	}
+
+}
---
+++ src/com/tsys/Test.java
@@ -0,0 +1,20 @@
+package com.tsys;
+
+
+class Test {
+	
+	int X = 10;
+
+	public static void main(String[] args) {
+
+		Test t = new Test();
+		
+		System.out.println(t.X);
+
+	}
+
+	}
+
+
+
+
