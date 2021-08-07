7775137ea0d87e18d6d3ea190d60bf073f57aa76
alles <alles@SRIKARA>
2021-08-07 15:34:27 +0530

    # WARNING: head commit changed in the meantime
    4/8

---
+++ src/com/tsys/SwitchUsingArray.java
@@ -0,0 +1,33 @@
+package com.tsys;
+
+public class SwitchUsingArray {
+
+	public static void main(String[] args) {
+			
+		String[] x = new String[] { "Mum", "Chen" , "Del" };
+		  
+		  
+		  System.out.println(x[0]); 
+		  System.out.println(x[1]); 
+		  System.out.println(x[2]);
+		  
+
+	/*	String city = "Pune";
+
+		switch(city) {
+
+		case "Del":
+
+			System.out.println("I AM IN DEL");
+
+		case "Chen":
+
+			System.out.println("I AM IN CHE");	
+
+		case "Pune":
+
+			System.out.println("I AM IN Pune")*/;
+
+		}
+	}
+
